6614f8081dd18ec9549fdb4d7e7fc98b27640835
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-05-17 00:20:48 -0400

    submission files

---
+++ submission/test.hpp
@@ -0,0 +1,24 @@
+/*
+ * test.hpp
+ *
+ *  Created on: May 6, 2022
+ *      Author: user
+ */
+#ifndef APPLICATION_USER_TEST_HPP_
+#define APPLICATION_USER_TEST_HPP_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+//int junk(int input);
+
+void initialize_vectors(int len, float lambda);
+bool detection(float datapoint,int tt);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APPLICATION_USER_TEST_HPP_ */
